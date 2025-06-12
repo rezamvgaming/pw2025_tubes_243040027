7cf1b413f3641a2c89fe43d9b7f779d2083fc408
rezamvgaming <rezamvgaming@gmail.com>
2025-06-12 20:15:52 +0700

    progress

--- css/style.php
+++ css/style.php
@@ -26,10 +26,261 @@
     }
 
     #home {
+        background-image: url(img/bg3.jpeg);
         display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
         gap: 2em;
+    }
+
+    #about {
+        background-color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 5em;
+    }
+
+    .about-box {
+        display: flex;
+        flex-direction: column;
+        text-align: center;
+        margin: 100px;
+    }
+
+    .about-info img {
+        padding-top: 100px;
+        min-height: 100px;
+        align-items: center;
+    }
+
+    .about-info h1 {
+        color: rgb(0, 0, 0);
+        text-align: center;
+    }
+
+    .about-box h1 {
+        font-size: 5em;
+    }
+
+    .about-box p {
+        color: white;
+        font-size: 30px;
+    }
+
+    .about-box h2 {
+        font-size: 2.5em;
+    }
+
+    #products {
+        background-color: #111;
+        padding: 60px 20px;
+        color: linear-gradient(to right, rgb(0, 0, 0), rgb(181, 201, 3));
+    }
+
+    .luxury-title {
+        font-family: "Poppins", sans-serif;
+        font-size: 2.5rem;
+        /* atau sesuai kebutuhan */
+        font-weight: 700;
+        background: linear-gradient(to right, rgb(0, 0, 0), rgb(181, 201, 3));
+        background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+
+    .product-card {
+        background-color: rgb(17, 15, 15);
+        border-radius: 12px;
+        overflow: hidden;
+        box-shadow: 0 4px 10px rgba(168, 197, 6, 0.5);
+        transition: transform 0.3s ease;
+    }
+
+    .product-card:hover {
+        transform: translateY(-5px);
+        box-shadow: 0 4px 10px yellow;
+
+    }
+
+    .product-card img {
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+    }
+
+    .product-card-body {
+        padding: 30px;
+    }
+
+    .product-card-body h5 {
+        font-size: 20px;
+        color: rgb(181, 201, 3);
+    }
+
+    .product-card-body p {
+        font-size: 14px;
+        color: #bbb;
+    }
+
+    .product-card-body .price {
+        color: #fff;
+        font-size: 18px;
+        font-weight: bold;
+    }
+
+    .container {
+        text-align: center;
+    }
+
+    .custom-button {
+        background-color: rgb(181, 201, 3);
+        color: black;
+        border: none;
+        padding: 10px 30px;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease, transform 0.2s ease;
+    }
+
+    .custom-button:hover {
+        background-color: rgb(0, 0, 0);
+        transform: scale(1.05);
+        color: rgb(181, 201, 3);
+    }
+
+    .custom-button:active {
+        transform: scale(0.95);
+    }
+
+    .custom-button2 {
+        background-color: rgb(107, 110, 107);
+        color: white;
+        border: none;
+        padding: 15px 50px;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease, transform 0.2s ease;
+    }
+
+    .custom-button2:hover {
+        background-color: #828680;
+        transform: scale(1.05);
+    }
+
+    .custom-button:active {
+        transform: scale(0.95);
+    }
+
+    #contact {
+        background-color: black;
+        text-align: center;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 2em;
+    }
+
+    form .input-box {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1em;
+    }
+
+    .info-box span {
+        font-size: 1.5em;
+    }
+
+    form input {
+        font-size: 1.3em;
+        padding: 1em 5em;
+        border-color: rgb(12, 11, 11, 0.4);
+        border-radius: 0.5em;
+        outline: none;
+    }
+
+    form input:focus {
+        background: rgb(229, 229, 229);
+    }
+
+    footer {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 10em;
+        bottom: 0;
+        background-color: rgb(12, 11, 11);
+        width: 100%;
+        padding: 5em 15%;
+    }
+
+    .col-left {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+
+    .col-box {
+        display: flex;
+        align-items: center;
+        gap: 1em;
+    }
+
+    .col-box i {
+        font-size: 2.5em;
+    }
+
+    .col-box span {
+        font-size: 1em;
+        margin: 10px;
+    }
+
+    .col-right {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+        color: white;
+        max-width: 500px;
+    }
+
+    .col-right span {
+        font-size: 1.3em;
+    }
+
+    .col-right .social-icons {
+        display: flex;
+        align-items: baseline;
+        justify-content: left;
+        gap: 1em;
+    }
+
+    .col-right .social-icons i {
+        color: rgba(184, 209, 184, 0.836);
+        font-size: 2em;
+        transition: 0.2s ease-in-out;
+        cursor: pointer;
+    }
+
+    .social-icons i:hover {
+        color: white;
+    }
+
+    .custom-button {
+        background-color: rgb(181, 201, 3);
+        color: black;
+        border: none;
+        padding: 10px 30px;
+        font-size: 16px;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease, transform 0.2s ease;
     }
 </style>
--- index.php
+++ index.php
@@ -16,9 +16,9 @@ require "css/style.php";
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Luxury Car</a>
+            <a class="navbar-brand" href="#">LuxuryCars</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -46,14 +46,228 @@ require "css/style.php";
 
     <section id="home">
         <div class="info-box">
-            <h2><span>Welcome to Luxury Cars</span></h2>
+            <h2><span class="ai">Welcome to Luxury Cars</span></h2>
         </div>
     </section>
 
     <section id="about">
-        <div class="about-info">
-            <p><span> Luxury Is Not a Choice, It's a Lifestyle</span>
+
+        <div class="about-box">
+
+            <div class="about-info">
+
+                <p><span>Luxury Is Not a Choice, it's a Lifestyle</span></p>
+            </div>
     </section>
+
+    <section id="products">
+        <div class="container">
+            <h2 class="text-center mb-5 luxury-title">Our Luxury Cars</h2>
+            <div class="row g-4">
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/porche911.jpeg">
+                        <div class="product-card-body">
+                            <h5>Porche911</h5>
+                            <p>Elegan, powerful, dan dilengkapi fitur terbaik di kelasnya.</p>
+                            <div class="price">7,9M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/m4.png">
+                        <div class="product-card-body">
+                            <h5>BMW M4 Competition</h5>
+                            <p>Desain futuristik dan kenyamanan luar biasa, di tambah look yang sporty.</p>
+                            <div class="price">3,1M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/svj.png">
+                        <div class="product-card-body">
+                            <h5>Lambhorghini Aventador SVJ</h5>
+                            <p>Teknologi canggih dan kemewahan tanpa kompromi.</p>
+                            <div class="price">22M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2 class="text-center mb-5">Our Luxury Cars</h2>
+            <div class="row g-4">
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/huracan.png">
+                        <div class="product-card-body">
+                            <h5>Lambhorghini Huracan</h5>
+                            <p>Elegan, powerful, dan dilengkapi fitur terbaik di kelasnya.</p>
+                            <div class="price">8,9M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/chiron.png">
+                        <div class="product-card-body">
+                            <h5>Bugatti Chiron</h5>
+                            <p>Desain futuristik dan kenyamanan luar biasa.</p>
+                            <div class="price">4,8M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/senna.png">
+                        <div class="product-card-body">
+                            <h5>Mclaren Senna</h5>
+                            <p>Teknologi canggih dan kemewahan tanpa kompromi.</p>
+                            <div class="price">14M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2 class="text-center mb-5">Our Luxury Cars</h2>
+            <div class="row g-4">
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/sf90.png">
+                        <div class="product-card-body">
+                            <h5>Ferarri SF90</h5>
+                            <p>Supercar Plug-in Hybrid Pertama dari Ferrari.”
+                                Inovatif, mewah, dan memukau dalam setiap detail.</p>
+                            <div class="price">17M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/r8.png">
+                        <div class="product-card-body">
+                            <h5>Audi R8</h5>
+                            <p>“Desain Futuristik, Jiwa Sportscar.”
+                                Rasakan sensasi mengemudi Audi R8 — mewah, cepat, berkelas.</p>
+                            <div class="price">9M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="product-card">
+                        <img src="img/car/senna.png">
+                        <div class="product-card-body">
+                            <h5>Bugatti Jesko</h5>
+                            <p>“Hypercar Langka, Performa Luar Biasa.”
+                                Miliki Jesko, simbol kekuatan dan eksklusivitas.</p>
+                            <div class="price">40M</div>
+                            <div class="container">
+                                <button class="custom-button">Beli</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+
+        <h1 class="header"><span>Contact</span></h1>
+
+        <form action="#">
+            <div class="input-box">
+                <span>Full Name</span>
+                <input type="text" placeholder="Enter Your Full Name">
+            </div>
+
+            <div class="input-box">
+                <span>Your Email</span>
+                <input type="text" placeholder="Enter Your Email">
+            </div>
+
+            <div class="input-box">
+                <span>Your Massage</span>
+                <input type="text" placeholder="Enter Your Massage">
+            </div>
+
+            <button class="custom-button" type="submit">Submit</button>
+
+        </form>
+    </section>
+
+    <footer>
+        <div class="col-left">
+            <div class="col-box">
+                <span><i class='bx bxs-location-plus'></i></span>
+                <span>Banjar,Jalan Cimenyan 1</span>
+            </div>
+
+            <div class="col-box">
+                <span><i class='bx bxs-phone'></i></span>
+                <span>089630477771- Ubay</span>
+            </div>
+
+            <div class="col-box">
+                <span><i class='bx bxs-envelope'></i></span>
+                <span>Nekooo-cell@gmail.com</span>
+            </div>
+        </div>
+
+        <div class="col-right">
+            <span>My Shop</span>
+            <p> BELANJA HANDPHONE TANPA RIBET!</p>
+            <p> Bisa cicilan</p>
+            <p> Bisa COD</p>
+            <p> Bisa kredit</p>
+            <p> Semua jadi lebih mudah!
+                Promo spesial untuk pembelian online dan offline.
+                Pilihan terbaik? Ya di Neko cell!</p>
+
+            <div class="social-icons">
+
+                <span>
+                    <a href="https://instagram.com/nekooo_cell">
+                        <i class='bx bxl-instagram-alt'></i>
+                    </a>
+                </span>
+                <span>
+                    <a href="https://whatsapp.com">
+                        <i class='bx bxl-whatsapp'></i>
+                    </a>
+                </span>
+            </div>
+        </div>
+    </footer>
 </body>
 
 </html>
